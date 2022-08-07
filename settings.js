58787f2d4ad0b67a1021b2ef26f2702d2e778cd9
farelfack <48388359+farelfack@users.noreply.github.com>
2022-08-08 00:19:34 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,14 +7,14 @@ global.APIs = {
 }
 
 // Other
-global.owner = ['6281575886399']
-global.premium = ['6281575886399']
+global.owner = ['6282249056319']
+global.premium = ['6282249056319']
 global.youtube = 'https://youtube.com/c/Riy'
-global.ownername = 'Riy'
+global.ownername = 'Kepin-X'
 global.botname ='Hinata-Md'
 global.donasi = {
     saweria: 'https://saweria.co/riy20',
-    nomor: '081575886399'
+    nomor: '082249056319'
 }
 global.packname = 'Hinata-Md'
 global.author = 'WhatsApp Bot'
